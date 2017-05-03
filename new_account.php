adf7979e824d9174998e696ec3aca20d0b8e1966
Ben Piecora <Ben Piecora>
2017-05-02 21:11:32 -0400

    initial commit

---
+++ new_account.php
@@ -0,0 +1,60 @@
+<html>
+<body>
+  <title>Test</title>
+
+  <?php
+
+  if($_POST["password"]!= $_POST["confirmpassword"]) {
+    echo "Check the password and try again...";
+  }
+
+  else {
+
+  $mysqli = new mysqli('127.0.0.1', 'root', '');
+
+  if ($mysqli->connect_error) {
+      die('Connect Error (' . $mysqli->connect_errno . ') '
+              . $mysqli->connect_error);
+  }
+  echo 'Connection OK';
+
+
+
+    if ($mysqli->select_db("Appadius") == TRUE) {
+      echo "Select DB Success";
+    }
+
+    else {
+      echo "Select DB Fail<br>" . $mysqli->error . "<br>";
+    }
+
+  $sql="INSERT INTO Users (Username, Password, Email) VALUES (
+    '" . $mysqli->escape_string($_POST["firstname"]) . "',
+    '" . $mysqli->escape_string($_POST["password"]) . "',
+    '" . $mysqli->escape_string($_POST["emailaddress"]) . "'
+
+  )";
+  echo "about to query";
+  if ($mysqli->query($sql) == TRUE) {
+    echo "Success";
+  }
+  else {
+    echo "you screwed up<br>" . $mysqli->error . "<br>";
+  }
+
+  $mysqli->close();
+
+  echo "<script> location.href='http://benstechgarage.com'; </script>";
+
+}
+
+
+  ?>
+<!--Debugging Purposes-->
+<!--Welcome <?php echo $_POST["firstname"]; ?><br>
+Your email address is: <?php echo $_POST["emailaddress"]; ?><br>
+The password you registered with is <?php echo $_POST["password"]; ?><br>
+Does your password match the confirmation? The confirmation password you entered is <?php echo $_POST['confirmpassword']; ?><br>-->
+
+</body>
+</html>
